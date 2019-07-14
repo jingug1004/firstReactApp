c2395331cc90e2f0c0b09a6d728614791061ccaa
jingug1004 <jingug1004@gmail.com>
2019-07-14 14:01:29 +0900

    https://github.com/jingug1004/firstReactApp.git

---
+++ src/MyComponent.js
@@ -0,0 +1,37 @@
+/**
+ * Created by A on 2019-07-14 오후 1:44
+ * hello-react /
+ * No pain, No gain!
+ * What :
+ * Why :
+ * How :
+ *
+ * @author ex) 공통서비스 개발팀 김진국
+ * @since ex) 2017/04/10
+ * @version 1.0
+ * @see
+ *
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ *
+ *   수정일      수정자          수정내용
+ *  -------    --------    ---------------------------
+ *  2017/04/21  홍길동          최초 생성
+ *  2017/05/27  이몽룡          인증이 필요없는 URL을 패스하는 로직 추가
+ *  </pre>
+ */
+
+
+import React, {Component} from 'react';
+
+class MyComponent extends Component {
+    render() {
+        return (
+            <div>
+                나의 새롭고 멋진 컴포넌트
+            </div>
+        )
+    }
+}
+
+export default MyComponent;
