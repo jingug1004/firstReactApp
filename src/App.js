2d32f49bd6d8e7ffdcb1215fb92f37cc8b0fab4c
jingug1004 <jingug1004@gmail.com>
2019-07-28 18:07:17 +0900

    190728 리액트를 다루는 기술

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MyComponent from './MyComponent';
+import EventPractice from './EventPractice';
 
 function App() {
     return (
         /*
         <MyComponent name="React"/>
         */
+        /*
         <MyComponent name={"React"} age={4}/>
+        */
+        <EventPractice/>
     );
 }
 
