f234e975b8b32d6277cc3a84422f2452e0963209
jingug1004 <jingug1004@gmail.com>
2019-07-30 21:59:25 +0900

    190730 리액트를 다루는 기술

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MyComponent from './MyComponent';
 import EventPractice from './EventPractice';
 import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
-function App() {
-    return (
-        /*
-        <MyComponent name="React"/>
-        */
-        /*
-        <MyComponent name={"React"} age={4}/>
-        */
-        /*
-        <EventPractice/>
-        */
-        <ValidationSample></ValidationSample>
-    );
+class App extends Component {
+    render() {
+        return (
+            /*
+            <MyComponent name="React"/>
+            */
+            /*
+            <MyComponent name={"React"} age={4}/>
+            */
+            /*
+            <EventPractice/>
+            */
+            /*
+            <ValidationSample></ValidationSample>
+            */
+            <div>
+                <ScrollBox
+                    ref={(ref) => this.scrollBox = ref}/>
+                <button onclick={() => this.scrollBox.scrollToBottom()}>
+                    맨 밑으로
+                </button>
+            </div>
+        );
+    }
 }
 
 export default App;
