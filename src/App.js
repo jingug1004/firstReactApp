f540fb07c3c8bf37ac6671b3370f889e69909d9f
jingug1004 <jingug1004@gmail.com>
2019-09-14 21:14:00 +0900

    190914 추석 리액트를 다루는 기술 Count.js 추가

--- src/App.js
+++ src/App.js
@@ -5,22 +5,24 @@ import MyComponent from './MyComponent';
 import EventPractice from './EventPractice';
 import ValidationSample from "./ValidationSample";
 import ScrollBox from "./ScrollBox";
+import Counter from './Counter';
 
-class App extends Component {
+/*class App extends Component {
     render() {
         return (
-            /*
+            /!*
             <MyComponent name="React"/>
-            */
-            /*
+            *!/
+            /!*
             <MyComponent name={"React"} age={4}/>
-            */
-            /*
+            *!/
+            /!*
             <EventPractice/>
-            */
-            /*
+            *!/
+            /!*
             <ValidationSample></ValidationSample>
-            */
+            *!/
+            /!*
             <div>
                 <ScrollBox
                     ref={(ref) => this.scrollBox = ref}/>
@@ -28,8 +30,14 @@ class App extends Component {
                     맨 밑으로
                 </button>
             </div>
+            *!/
+
         );
     }
+}*/
+
+const App = () => {
+    return <Counter/>;
 }
 
 export default App;
