f325ea5c08806da5286d8426ee41617a79362335
jingug1004 <jingug1004@gmail.com>
2019-09-20 20:36:19 +0900

    190920(금) 리액트를 다루는 기술_01 // LifeCycleSample.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ScrollBox from "./ScrollBox";
 import Counter from './Counter';
 import Say from './Say';
 import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
 
 /*class App extends Component {
     render() {
@@ -38,11 +39,28 @@ import IterationSample from "./IterationSample";
     }
 }*/
 
-class App extends Component{
-    render () {
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+    state = {
+        color: '#000000'
+    }
+
+    handleClick = () => {
+        this.setState({
+            color: getRandomColor()
+        });
+    }
+
+    render() {
         return (
-        <IterationSample/>
-    );
+            <div>
+                <button onClick={this.handleClick}>랜덤 색상</button>
+                <LifeCycleSample color={this.state.color}/>
+            </div>
+        );
     }
 }
 
